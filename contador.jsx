efc85da48dd34cb47d91daa26d2cbc8f7757e5a0
mrhe1303 <mrhe1303@gmail.com>
2025-08-06 16:09:52 -0500

    Create contador.jsx

---
+++ contador.jsx
@@ -0,0 +1,38 @@
+const { useState, useEffect } = React;
+
+function SecondsCounter({ segundos }) {
+    const segStr = segundos.toString().padStart(6, '0');
+    const digitos = segStr.split('').map(Number);
+
+    return (
+        <div className='barra'>
+            <div className='caja1'>
+                <i className="fa-solid fa-clock reloj"></i>
+            </div>
+            {digitos.map((digito, index) => (
+                <div key={index} className={`caja${index + 2}`}>
+                    <h1>{digito}</h1>
+                </div>
+            ))}
+        </div>
+    );
+}
+
+function App() {
+    const [segundos, conteoSegundos] = useState(0);
+
+    useEffect(() => {
+        const intervalo = setInterval(() => {
+            conteoSegundos(viejosSegundos => {
+                const nuevosSegundos = viejosSegundos < 59 ? viejosSegundos + 1 : 59;
+                return nuevosSegundos;
+            });
+        }, 1000);
+
+        return () => clearInterval(intervalo);
+    }, []);
+    return <SecondsCounter segundos={segundos} />;
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
